e8b3f8b26666e8de20493d1d3cde94323a56982c
Carro <robertmyers@hey.com>
2023-07-27 17:34:22 -0500

    a

--- server/text_generation_server/models/btlm.py
+++ server/text_generation_server/models/btlm.py
@@ -64,7 +64,7 @@ class BTLMSharded(CausalLM):
 
         torch.distributed.barrier(group=self.process_group)
         super(CausalLM, self).__init__(
-            model=model.to(self.device),
+            model=model.to(device),
             tokenizer=tokenizer,
             requires_padding=True,
             dtype=dtype,
